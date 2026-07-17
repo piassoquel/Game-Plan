ef37caea58ca071756346f613e6f6f303b1a4804
piassoquel <store@playitagainsoquel.com>
2026-07-17 08:28:06 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.GAMEPLAN_CONFIG = {
   environment: "production",
-  apiBaseUrl: "https://script.google.com/macros/s/AKfycbx-u67jjytr86lvixlqQzX9Xpit7fcOHb3Z0_ahjHyqXu_lPY3wXnzsj4-16kA4T0Tl/exec",
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbztbAzEgpSOA9b3pqXoyVIxL0exQ3sw_7IYTryAHPxAmhzQJPxi_ZmKFTVV_6Ji3JqV/exec",
   placesApiKey: ""
 };
